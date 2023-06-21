eb9adbe47d8081523a5a496ed1513ab996a5d293
Bryan Langhendries <bryan1999langhendries@hotmail.com>
2023-06-21 18:52:04 +0200

    added navbar animations
    small changes to main

--- demannekesfc/src/components/nav/Nav.js
+++ demannekesfc/src/components/nav/Nav.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 export default function Nav() {
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos === 0);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
   return (
-    <nav className="navbar">
-      <a href="/" id="navbar-logo">PORTFOLIO</a>
+    <nav className={`navbar ${visible ? "" : "hidden"}`}>
+      <a href="/" id="navbar-logo">
+        PORTFOLIO
+      </a>
       <div className="navbar-links">
-      <a href="/">Home</a>
-      <a href="/">About</a>
-      <a href="/">Porfolio</a>
-      <a href="/">Services</a>
-      <a href="/">Contact</a>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
       </div>
     </nav>
   );

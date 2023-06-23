6b9617ec26c79927542f927abc7436c99a73223a
MichaelFJ98 <michael.fernandez-juarez@student.ehb.be>
2023-06-23 22:22:36 +0200

    added About updates

--- demannekesfc/src/components/about/About.js
+++ demannekesfc/src/components/about/About.js
@@ -3,7 +3,16 @@ import "./About.css";
 export default function About() {
   return (
     <div id="about">
-      <h1>ABOUT</h1>
+      <div id="titles">
+        <p>Dreams To Reality</p>
+        <h1>Hi, We are Bryan And Michael</h1>
+        <h1>Full Stack Developers</h1>
+        <h1>Based in Belgium.</h1>
+      </div>
+      <div id="pictures">
+        <img src="/assets/images/Bryan.png" alt="Bryan" width="200px"></img>
+        <img src="/assets/images/Michel.png" alt="Michael" width="200px"></img>
+      </div>
     </div>
   );
 }

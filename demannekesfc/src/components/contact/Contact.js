d6856c4c042163097d7a3bac461049a762400272
Bryan Langhendries <bryan1999langhendries@hotmail.com>
2023-06-26 18:37:53 +0200

    Updated Contact

--- demannekesfc/src/components/contact/Contact.js
+++ demannekesfc/src/components/contact/Contact.js
@@ -1,7 +1,8 @@
 import "./Contact.css";
 
-export default function Contact() {
+import Form from "../Form.js";
 
+export default function Contact() {
   const handleEmailClick = () => {
     const emailAddress = "info@mannekes.be";
     navigator.clipboard.writeText(emailAddress);
@@ -13,29 +14,70 @@ export default function Contact() {
   };
 
   return (
-    <div id="contact">
-      <div id="text">
-        <h2>Let's build</h2>
-        <h2>something</h2>
-        <h1>great together.</h1>
-      </div>
-      <div id="form">
-        <input id="name" type="text" placeholder="Name"></input>
-        <input id="email" type="email" placeholder="Email"></input>
-        <input id="company-name" type="text" placeholder="Company Name"></input>
-        <input id="phone" type="tel" placeholder="Phone Number"></input>
-        <button>SEND IN</button>
-      </div>
-
-      <div id="info">
-        <small>EMAIL</small>
-        <p onClick={handleEmailClick}>Info@mannekes.be</p>
-        <small>INSTAGRAM</small>
-        <p onClick={handleInstagramClick}>@Mannekes</p>
-      </div>
-      <div id="logo">
-        <img src="/assets/images/Logo.png"></img>
-      </div>
+    <div
+      id="contact"
+      class="h-screen bg-white dark:bg-gray-900"
+    >
+        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+            Contact Us
+          </h2>
+          <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+            Let's build something great together.
+          </p>
+          <form action="#" class="space-y-8">
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="name@email.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                for="subject"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Let us know how we can help you"
+                required
+              />
+            </div>
+            <div class="sm:col-span-2">
+              <label
+                for="message"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Your message
+              </label>
+              <textarea
+                id="message"
+                rows="6"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Leave a comment..."
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Send message
+            </button>
+          </form>
+        </div>
     </div>
   );
 }

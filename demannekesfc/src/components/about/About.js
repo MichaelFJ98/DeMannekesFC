bebbb43995baa59d2f5e31331e5cb19bb9187345
MichaelFJ98 <michael.fernandez-juarez@student.ehb.be>
2023-06-26 18:48:20 +0200

    Updated about page (unfinished)

--- demannekesfc/src/components/about/About.js
+++ demannekesfc/src/components/about/About.js
@@ -4,20 +4,23 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 
 
+
 export default function Example() {
 
   return (
-    <div className="flex bg-zinc-100 h-screen flex-row items-center justify-around">
-      <div className=''>
-        <p className='text-sm font-semibold leading-6 text-gray-400'>Dreams To Reality</p>
-        <h1 className='text-2xl font-bold tracking-tight text-stone-950 sm:text-6xl'>Hi, We are Bryan and Michael</h1>
-        <h1 className='text-2xl font-bold tracking-tight text-blue-500 sm:text-6xl'>Full-Stack Developers</h1>
-        <h1 className='text-2xl font-bold tracking-tight  text-stone-950 sm:text-6xl'>Based In Belgium.</h1>
-        <button class="bg-transparent hover:bg-blue-500 text-stone-950 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Button
-        </button>
-        <button></button>
+    <div className="flex bg-zinc-100 h-screen flex-col items-center justify-center gap-6">
+      <div className='text-center'>
+        <p className='text-sm font-sans font-medium italic leading-6 text-gray-400 '>Dreams to Reality</p>
+        <h1 className='text-2xl font-bold tracking-tight text-stone-950 sm:text-6xl '>Hi, We are Bryan and Michael</h1>
+        <h1 className='py-2 text-2xl font-mono font-bold tracking-tight text-blue-500 sm:text-6xl overflow-hidden whitespace-nowrap animate-typing border-r-2 border-black'>Full-Stack Developers</h1>
+        <h1 className='text-2xl font-bold tracking-tight  text-stone-950 sm:text-6xl'>Based in Belgium.</h1>
       </div> 
+      <div className='flex flex-row gap-4 items-center'>
+        <button class="bg-blue-500 font-semibold transition-colors shadow-lg duration-300 ease-in-out hover:text-blue-500 px-4 border-2 border-blue-500 hover:bg-transparent rounded h-10">
+        Hire us
+        </button>
+        <a href="#" class="text-base font-semibold text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+      </div>
     </div>
   )
 }
--- demannekesfc/tailwind.config.js
+++ demannekesfc/tailwind.config.js
@@ -2,7 +2,45 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    patterns: {
+      opacities: {
+          100: "1",
+          80: ".80",
+          60: ".60",
+          40: ".40",
+          20: ".20",
+          10: ".10",
+          5: ".05",
+      },
+      sizes: {
+          1: "0.25rem",
+          2: "0.5rem",
+          4: "1rem",
+          6: "1.5rem",
+          8: "2rem",
+          16: "4rem",
+          20: "5rem",
+          24: "6rem",
+          32: "8rem",
+      }
   },
-  plugins: [],
+    extend: {
+      animation: {
+        typing: "typing 1.7s steps(21), blink .9s infinite"
+      },
+      keyframes: {
+        typing: {
+          from: { width: "0" },
+          to: { width: "21ch" }
+        },
+        blink: {
+          from: { "border-right-color": "black" },
+          to: { "border-right-color": "transparent" },
+        },
+      }
+    },
+  },
+  plugins: [
+    require('tailwindcss-bg-patterns'),
+  ],
 }

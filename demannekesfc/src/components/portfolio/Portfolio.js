ac21aa4a51243a43d52fa18f6bbfc113d81a2f8f
MichaelFJ98 <michael.fernandez-juarez@student.ehb.be>
2023-07-03 16:52:48 +0200

    Big Update website overhaul

--- demannekesfc/src/components/portfolio/Portfolio.js
+++ demannekesfc/src/components/portfolio/Portfolio.js
@@ -2,8 +2,46 @@ import "./Portfolio.css";
 
 export default function Portfolio() {
     return (
-      <div id="portfolio">
-        <h1>PORTFOLIO</h1>
+      <div id="portfolio" className="  bg-blue-50 space-y-6 p-12">
+        
+        <h1 className="text-2xl font-bold tracking-tight text-stone-950 sm:text-6xl pb-6 text-center">Our Projects</h1>
+
+        <div id="portfolios" className="flex flex-row justify-around">
+
+          <div id="pf1" className="drop-shadow-lg ">
+            <div id="circleImg" className='w-96 h-112 rounded-t-lg overflow-hidden'>
+              <img src='./assets/images/template1.jpg' className=''></img>
+            </div>
+            <div id="textArea" className=" bg-blue-950 w-96 p-4" >
+              <h3 className='text-blue-300'>Template 1</h3>
+              <p className='text-sm font-sans font-medium leading-6 text-gray-50'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis egestas maecenas pharetra convallis posuere morbi leo urna.</p>
+              <a href="#" class="text-base font-semibold text-gray-50 underline hover:text-blue-300 hover:no-underline">See more <span aria-hidden="true">→</span></a>
+            </div>
+          </div>
+
+          <div id="pf2" className="drop-shadow-lg">
+            <div id="circleImg" className='w-96 h-112 rounded-t-lg overflow-hidden'>
+              <img src='./assets/images/template1.jpg' className=''></img>
+            </div>
+            <div id="textArea" className=" bg-blue-950 w-96 p-4" >
+              <h3 className='text-blue-300'>Template 2</h3>
+              <p className='text-sm font-sans font-medium leading-6 text-gray-50'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis egestas maecenas pharetra convallis posuere morbi leo urna.</p>
+              <a href="#" class="text-base font-semibold text-gray-50 underline hover:text-blue-300 hover:no-underline">See more <span aria-hidden="true">→</span></a>
+            </div>
+          </div>
+
+          <div id="pf3" className="drop-shadow-lg">
+            <div id="circleImg" className='w-96 h-112 rounded-t-lg overflow-hidden'>
+              <img src='./assets/images/template1.jpg' className=''></img>
+            </div>
+            <div id="textArea" className=" bg-blue-950 w-96 p-4" >
+              <h3 className='text-blue-300'>Template 3</h3>
+              <p className='text-sm font-sans font-medium leading-6 text-gray-50'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis egestas maecenas pharetra convallis posuere morbi leo urna.</p>
+              <a href="#" class="text-base font-semibold text-gray-50 underline hover:text-blue-300 hover:no-underline">See more <span aria-hidden="true">→</span></a>
+            </div>
+          </div>
+
+        </div>
       </div>
     );
   }
--- demannekesfc/src/components/services/Services.js
+++ demannekesfc/src/components/services/Services.js
@@ -5,20 +5,41 @@ import {BsGraphUpArrow} from "react-icons/bs";
 
 export default function Services() {
   return (
-    <div className="services">
-      <div id="text">
-        <h1>Services</h1>
-      </div>
-      <div id="list">
-        <div>
-          <FaPaintbrush className="logo"/>
-          <h5>Web Design</h5>
-          <p>lorem ipsum dolor sit amet consectetur</p>
+    <div id="Skills" className="bg-blue-950 p-12">
+      <h1 className="text-2xl font-bold tracking-tight text-blue-50 sm:text-6xl text-center pb-6">Our Skillset</h1>
+      
+      <div id="skillBox" className="flex flex-row justify-around">
+        <div id="Frontend" className="bg-blue-50 w-96 h-96 min-h-max rounded-lg text-stone-950 p-4 drop-shadow-lg ">
+          <h1 className="text-2xl font-bold tracking-tight text-stone-950 sm:text-4xl  text-center">Frontend</h1>
+          <ul className="text-center">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Tailwind CSS</li>
+            <li>Boostrap</li>
+          </ul>
         </div>
-        <div>
-            <BsGraphUpArrow className="logo"/>
-          <h5>Advertising</h5>
-          <p>lorem ipsum dolor sit amet consectetur</p>
+
+        <div id="Back-end" className="bg-blue-50 w-96 h-96 min-h-max rounded-lg text-stone-950 p-4 drop-shadow-lg ">
+          <h1 className="text-2xl font-bold tracking-tight text-stone-950 sm:text-4xl  text-center">Back-end</h1>
+          <ul className="text-center">
+            <li>.NET</li>
+            <li>NodeJS</li>
+            <li>Java</li>
+            <li>Python</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>FireBase</li>
+          </ul>
+        </div>
+
+        <div id="Other" className="bg-blue-50 w-96 h-96 min-h-max rounded-lg text-stone-950 p-4 drop-shadow-lg ">
+          <h1 className="text-2xl font-bold tracking-tight text-stone-950 sm:text-4xl  text-center">Other</h1>
+          <ul className="text-center">
+            <li>Git</li>
+            <li>Rest API</li>
+          </ul>
         </div>
       </div>
     </div>

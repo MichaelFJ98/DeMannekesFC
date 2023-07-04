5dfba1e04a781be5aa5788624f097a908379dbe6
Bryan Langhendries <bryan1999langhendries@hotmail.com>
2023-07-04 23:24:49 +0200

    Update Services.js
    Can now use onClick to show all the skills

--- demannekesfc/src/components/services/Services.js
+++ demannekesfc/src/components/services/Services.js
@@ -2,56 +2,84 @@ import { GoCommandPalette } from "react-icons/go";
 import { FaLaptopCode } from "react-icons/fa";
 import { FaTools } from "react-icons/fa";
 
-import { TfiHtml5 } from "react-icons/tfi";
-import { FaCss3Alt } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaReact, FaBootstrap } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
-import { FaReact } from "react-icons/fa";
 import { SiTailwindcss } from "react-icons/si";
-import { FaBootstrap } from "react-icons/fa";
+import { useState } from "react";
+
+const frontIcons = {
+  HTML: FaHtml5,
+  CSS: FaCss3Alt,
+  Javascript: IoLogoJavascript,
+  React: FaReact,
+  Tailwind: SiTailwindcss,
+  Bootstrap: FaBootstrap,
+};
+
+const backIcons = {
+};
+
+const toolIcons = {
+};
 
 export default function Services() {
+  const [selectedSkill, setSelectedSkill] = useState("front");
+
   return (
-    <div id="skills" className="bg-blue-950 flex flex-row h-screen">
-      <div className="h-full w-1/3 flex flex-col text-white border-r-2">
-        <div className="w-full h-1/4 flex flex-col justify-center items-center">
-          <FaLaptopCode className="text-7xl" />
+    // Main container
+    <div id="skills" className="bg-blue-950 flex flex-col items-center">
+      {/* Title */}
+      <div className="text-white text-6xl w-full text-center py-4">
+        <h1>SKILLS</h1>
+      </div>
+      {/* Skill list */}
+      <div className="flex flex-row text-white justify-around w-full pb-4 ">
+        <div onClick={() => setSelectedSkill("front")} className="flex flex-col justify-center items-center cursor-pointer">
+          <FaLaptopCode className="text-9xl" />
           <p>Front End</p>
         </div>
-        <div className="w-full h-1/4 flex flex-col justify-center items-center">
-          <GoCommandPalette className="text-7xl" />
+        <div onClick={() => setSelectedSkill("back")} className="flex flex-col justify-center items-center cursor-pointer">
+          <GoCommandPalette className="text-9xl" />
           <p>Back End</p>
         </div>
-        <div className="w-full h-1/4 flex flex-col justify-center items-center">
-          <FaTools className="text-7xl" />
+        <div onClick={() => setSelectedSkill("tools")} className="flex flex-col justify-center items-center cursor-pointer">
+          <FaTools className="text-9xl" />
           <p>Tools</p>
         </div>
       </div>
-      <div className="text-white h-full w-1/2 flex flex-row flex-wrap justify-auto content-start p-4">
-        <div className="w-40 h-40 border flex flex-col justify-center items-center m-2">
-          <TfiHtml5 className="text-7xl" />
-          <p>HTML</p>
-        </div>
-        <div className="w-40 h-40 border flex flex-col justify-center items-center m-2">
-          <FaCss3Alt className="text-7xl" />
-          <p>CSS</p>
-        </div>
-        <div className="w-40 h-40 border flex flex-col justify-center items-center m-2">
-          <IoLogoJavascript className="text-7xl" />
-          <p>Javascript</p>
-        </div>
-        <div className="w-40 h-40 border flex flex-col justify-center items-center m-2">
-          <FaReact className="text-7xl" />
-          <p>React</p>
-        </div>
-        <div className="w-40 h-40 border flex flex-col justify-center items-center m-2">
-          <SiTailwindcss className="text-7xl" />
-          <p>Tailwind</p>
-        </div>
-        <div className="w-40 h-40 border flex flex-col justify-center items-center m-2">
-          <FaBootstrap className="text-7xl" />
-          <p>Bootstrap</p>
-        </div>
-      </div>
+      {/* Border */}
+      <div className="border-2 w-full"></div>
+      {/* SubSkills */}
+      <ShowSkills selectedSkill={selectedSkill} />
+    </div>
+  );
+}
+
+function ShowSkills(props) {
+  const { selectedSkill } = props;
+
+  // Access the icon dictionary based on the selected skill
+  const icons = {
+    front: frontIcons,
+    back: backIcons,
+    tools: toolIcons,
+  }[selectedSkill];
+
+  if (!icons) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-row justify-around items-center text-white">
+      {Object.keys(icons).map((name) => {
+        const Icon = icons[name];
+        return (
+          <div key={name} className="flex flex-col justify-evenly items-center">
+            {Icon && <Icon className="text-5xl" />}
+            <p>{name}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }

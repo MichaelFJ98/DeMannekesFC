72ee8dd5ed44ab0c3ab3c58077fb988f824db496
Bryan Langhendries <bryan1999langhendries@hotmail.com>
2023-07-05 12:07:27 +0200

    Update Services.js
    Made it responsive & added backend and tools icons

--- demannekesfc/src/components/services/Services.js
+++ demannekesfc/src/components/services/Services.js
@@ -1,25 +1,47 @@
+// Skill Icons
 import { GoCommandPalette } from "react-icons/go";
 import { FaLaptopCode } from "react-icons/fa";
 import { FaTools } from "react-icons/fa";
 
+// Front End Icons
 import { FaHtml5, FaCss3Alt, FaReact, FaBootstrap } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { SiTailwindcss } from "react-icons/si";
+
+// Back End Icons
+import { SiDotnet, SiExpress, SiMongodb, SiFirebase } from "react-icons/si";
+import { FaNodeJs, FaJava, FaPython } from "react-icons/fa";
+
+
+// Tools Icons
+import { FaGit } from "react-icons/fa";
+import { TbApi } from "react-icons/tb";
+
+
 import { useState } from "react";
 
 const frontIcons = {
   HTML: FaHtml5,
   CSS: FaCss3Alt,
-  Javascript: IoLogoJavascript,
+  JavaScript: IoLogoJavascript,
   React: FaReact,
-  Tailwind: SiTailwindcss,
+  TailwindCSS: SiTailwindcss,
   Bootstrap: FaBootstrap,
 };
 
 const backIcons = {
+  DotNet: SiDotnet,
+  Express: SiExpress,
+  MongoDB: SiMongodb,
+  Firebase: SiFirebase,
+  NodeJS: FaNodeJs,
+  Java: FaJava,
+  Python: FaPython,
 };
 
 const toolIcons = {
+  Git: FaGit,
+  RESTAPI: TbApi,
 };
 
 export default function Services() {
@@ -29,21 +51,36 @@ export default function Services() {
     // Main container
     <div id="skills" className="bg-blue-950 flex flex-col items-center">
       {/* Title */}
-      <div className="text-white text-6xl w-full text-center py-4">
+      <div className="text-white text-3xl md:text-6xl w-full text-center py-4">
         <h1>SKILLS</h1>
       </div>
       {/* Skill list */}
-      <div className="flex flex-row text-white justify-around w-full pb-4 ">
-        <div onClick={() => setSelectedSkill("front")} className="flex flex-col justify-center items-center cursor-pointer">
-          <FaLaptopCode className="text-9xl" />
+      <div className="flex flex-col md:flex-row text-white justify-around w-full pb-4 ">
+        <div
+          onClick={() => setSelectedSkill("front")}
+          className={`flex flex-col justify-center items-center cursor-pointer ${
+            selectedSkill === "front" ? "border-2 rounded p-2" : ""
+          }`}
+        >
+          <FaLaptopCode className="text-6xl md:text-9xl" />
           <p>Front End</p>
         </div>
-        <div onClick={() => setSelectedSkill("back")} className="flex flex-col justify-center items-center cursor-pointer">
-          <GoCommandPalette className="text-9xl" />
+        <div
+          onClick={() => setSelectedSkill("back")}
+          className={`flex flex-col justify-center items-center cursor-pointer ${
+            selectedSkill === "back" ? "border-2 rounded p-2" : ""
+          }`}
+        >
+          <GoCommandPalette className="text-6xl md:text-9xl" />
           <p>Back End</p>
         </div>
-        <div onClick={() => setSelectedSkill("tools")} className="flex flex-col justify-center items-center cursor-pointer">
-          <FaTools className="text-9xl" />
+        <div
+          onClick={() => setSelectedSkill("tools")}
+          className={`flex flex-col justify-center items-center cursor-pointer ${
+            selectedSkill === "tools" ? "border-2 rounded p-2" : ""
+          }`}
+        >
+          <FaTools className="text-6xl md:text-9xl" />
           <p>Tools</p>
         </div>
       </div>
@@ -70,13 +107,13 @@ function ShowSkills(props) {
   }
 
   return (
-    <div className="flex flex-row justify-around items-center text-white">
+    <div className="flex flex-row flex-wrap justify-around items-center text-white w-full py-4">
       {Object.keys(icons).map((name) => {
         const Icon = icons[name];
         return (
-          <div key={name} className="flex flex-col justify-evenly items-center">
-            {Icon && <Icon className="text-5xl" />}
-            <p>{name}</p>
+          <div key={name} className="flex flex-col items-center">
+            {Icon && <Icon className="text-5xl mb-2" />}
+            <p className="hidden sm:block">{name}</p>
           </div>
         );
       })}

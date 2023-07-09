2981168c9f997d6e6e3f63f351d7fe96ff822f4a
MichaelFJ98 <michael.fernandez-juarez@student.ehb.be>
2023-07-09 16:23:27 +0200

    Updated Text

--- demannekesfc/src/components/home/Home.js
+++ demannekesfc/src/components/home/Home.js
@@ -4,8 +4,8 @@ export default function Home() {
     <div id="home" className="flex h-screen flex-col items-center justify-center gap-6">
       <div className='text-center'>
         
-        <p className='text-sm font-sans font-medium italic leading-6 text-gray-400 '>Dreams to Reality</p>
-        <h1 className=' text-2xl font-bold tracking-tight text-stone-950 sm:text-6xl '>Hi, We are Bryan and Michael</h1>
+        <p className='text-sm font-sans font-medium italic leading-6 text-gray-400 '>Bringing your dream website to life, one pixel at a time.</p>
+        <h1 className=' text-2xl font-bold tracking-tight text-stone-950 sm:text-6xl '>Hi, We are InSiter</h1>
         <h1 className='inline-block overflow-hidden whitespace-nowrap font-mono font-bold text-2xl py-2 tracking-tight text-blue-500 sm:text-6xl animate-typing border-r-2 border-black'>Full-Stack Developers</h1>
         <h1 className='text-2xl font-bold tracking-tight  text-stone-950 sm:text-6xl'>Based in Belgium.</h1>
       </div> 

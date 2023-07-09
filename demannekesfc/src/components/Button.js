b58787fdeaf10563fc94b7e403393f62d17bc413
Bryan Langhendries <bryan1999langhendries@hotmail.com>
2023-07-09 22:51:53 +0200

    Update Button.js

--- demannekesfc/src/components/Button.js
+++ demannekesfc/src/components/Button.js
@@ -10,7 +10,7 @@ export default function Button({ text, fontSize, onClick, disabled }) {
   return (
     <div>
       <button
-        className={`bg-blue-500 text-white ${fontSize} transition-colors shadow-lg duration-300 ease-in-out hover:text-blue-500 px-4 border-2 border-blue-500 hover:bg-transparent rounded h-10`}
+        className={`bg-blue-500 text-white ${fontSize} transition-all shadow-lg duration-300 ease-in-out hover:scale-110 hover:text-blue-500 px-4 border-2 border-blue-500 hover:bg-transparent rounded h-10`}
         onClick={handleClick}
         disabled={disabled}
       >

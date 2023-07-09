1411e7a6c98ed98a50522c345b576f8d070be279
Bryan Langhendries <bryan1999langhendries@hotmail.com>
2023-07-09 21:38:09 +0200

    updated font

--- demannekesfc/src/components/home/Home.js
+++ demannekesfc/src/components/home/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className='text-center'>
         
         <p className='text-sm font-sans font-medium italic leading-6 text-gray-400 '>Bringing your dream website to life, one pixel at a time.</p>
-        <h1 className=' text-2xl font-bold tracking-tight text-stone-950 sm:text-6xl '>Hi, We are InSiter</h1>
+        <h1 className=' text-2xl font-bold tracking-tight text-stone-950 sm:text-6xl '>Hi, We are Insiter</h1>
         <h1 className='inline-block overflow-hidden whitespace-nowrap font-mono font-bold text-2xl py-2 tracking-tight text-blue-500 sm:text-6xl animate-typing border-r-2 border-black'>Full-Stack Developers</h1>
         <h1 className='text-2xl font-bold tracking-tight  text-stone-950 sm:text-6xl'>Based in Belgium.</h1>
       </div> 

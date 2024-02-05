23fdfe4a42912a7d509d6ac17981e722199a3377
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2024-02-05 17:35:32 +0100

    Update Tier2.js

--- demannekesfc/src/components/tiers/Tier2.js
+++ demannekesfc/src/components/tiers/Tier2.js
@@ -6,7 +6,7 @@ export default function Tier2() {
         <div className="bg-c3 h-full xl:h-[45rem]" id="packages">
             <div className="bg-c2 w-screen h-full flex lg:justify-center p-4 lg-p-0 rounded-2xl">
                 <div className="flex flex-col lg:flex-row lg:w-10/12 h-full items-center lg:space-x-20 p-4">
-                    <img src="./assets/images/hairdresser/herobg.webp" alt="barberimg" className="hidden md:hidden lg:block xl:block w-full lg:w-[30rem] xl:w-[40rem] rounded-md object-cover" />
+                    
                     <div className="w-full lg:w-[38rem] flex flex-col space-y-6">
                         <Fade top distance="20px" duration={1500}>
                             <h1 className="text-4xl font-semibold lg:font-bold text-l1 tracking-wide lg:text-left">Tier 2 package</h1>
@@ -17,7 +17,9 @@ export default function Tier2() {
                         <Fade top distance="20px" duration={1500} delay={600}>
                             <button className="bg-primary hover:bg-blue-600 hover:scale-110 transition-all shadow-lg duration-300 p-4 rounded-2xl text-l1 font-semibold lg:mx-0">View example website</button>
                         </Fade>
+                        
                     </div>
+                    <img src="./assets/images/hairdresser/herobg.webp" alt="barberimg" className="hidden md:hidden lg:block xl:block w-full lg:w-[30rem] xl:w-[40rem] rounded-md object-cover" />
                 </div>
             </div>
         </div>

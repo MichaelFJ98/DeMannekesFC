3e22ad0ed3b8c987a861750ed25d05d326a080a9
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2024-02-06 14:54:16 +0100

    revamp tiers

--- demannekesfc/src/components/tiers/Tier1.js
+++ demannekesfc/src/components/tiers/Tier1.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
+import { FaCheck } from "react-icons/fa";
+
+const features = [
+    "Base website",
+    "SSL certificate",
+    "SEO optimization",
+    "1<sup>st</sup> month free hosting",
+  ];
 
 export default function Tier1() {
     return (
         <div className="bg-l1 h-full xl:h-[45rem]" id="packages">
             <div className="bg-c3 rounded-t-[40px] w-screen h-full flex lg:justify-center p-4 lg-p-0">
-                <div className="flex flex-col md:flex-row lg:w-10/12 h-full items-center md:space-x-20 p-4">
-                    <img src="./assets/images/hairdresser/herobg.webp" alt="barberimg" className="hidden md:block xl:block w-full lg:w-[30rem] xl:w-[40rem] rounded-md object-cover" />
-                    <div className="w-full lg:w-[38rem] flex flex-col space-y-6">
+                <div className="flex flex-col md:flex-row lg:w-10/12 h-full items-center justify-around md:space-x-20 p-4">
+                    <img src="./assets/images/hairdresser/herobg.webp" alt="barberimg" className="hidden md:block w-full lg:w-[30rem] xl:w-[45rem] rounded-md object-cover" />
+                    <div className="w-full lg:w-[38rem] flex flex-col space-y-8">
                         <Fade top distance="20px" duration={1500}>
                             <h1 className="text-4xl font-semibold lg:font-bold text-l1 tracking-wide lg:text-left">Tier 1 Package</h1>
                         </Fade>
                         <Fade top distance="20px" duration={1500} delay={300}>
                             <p className="text-l2 leading-7 lg:text-left">Introducing our premier package, featuring a foundational website seamlessly integrated with an advanced reservation system. Elevate your experience with the assurance of securing the perfect products tailored just for you. Discover the ultimate solution for a streamlined and efficient online presence.</p>
+                            <ul className="list-none space-y-2">
+                                {features.map((feature, index) => (
+                                    <li key={index} className="flex items-center space-x-2 text-white">
+                                        <FaCheck className="text-primary" />
+                                        <p dangerouslySetInnerHTML={{ __html: feature }}></p>
+                                    </li>
+                                ))}
+                            </ul>
                         </Fade>
                         <Fade top distance="20px" duration={1500} delay={600}>
                             <button className="bg-primary hover:bg-blue-600 hover:scale-110 transition-all shadow-lg duration-300 p-4 rounded-2xl text-l1 font-semibold lg:mx-0">View example website</button>
--- demannekesfc/src/components/tiers/Tier2.js
+++ demannekesfc/src/components/tiers/Tier2.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
+import { FaCheck } from "react-icons/fa";
+
+const features = [
+    "All features from Tier 1",
+    "Fully automated reservation system",
+    "Social media integration",
+    "Google reviews integration",
+  ];
 
 export default function Tier2() {
     return (
         <div className="bg-c3 h-full xl:h-[45rem]" id="packages">
             <div className="bg-c2 w-screen h-full flex lg:justify-center p-4 lg-p-0 rounded-2xl">
-                <div className="flex flex-col md:flex-row lg:w-10/12 h-full items-center md:space-x-20 p-4">
+                <div className="flex flex-col md:flex-row lg:w-10/12 h-full justify-around items-center md:space-x-20 p-4">
                     
-                    <div className="w-full lg:w-[38rem] flex flex-col space-y-6">
+                    <div className="w-full lg:w-[38rem] flex flex-col space-y-8">
                         <Fade top distance="20px" duration={1500}>
                             <h1 className="text-4xl font-semibold lg:font-bold text-l1 tracking-wide lg:text-left">Tier 2 Package</h1>
                         </Fade>
                         <Fade top distance="20px" duration={1500} delay={300}>
                             <p className="text-l2 leading-7 lg:text-left">Introducing our premier package, featuring a foundational website seamlessly integrated with an advanced reservation system. Elevate your experience with the assurance of securing the perfect products tailored just for you. Discover the ultimate solution for a streamlined and efficient online presence.</p>
+                            <ul className="list-none space-y-2">
+                                {features.map((feature, index) => (
+                                    <li key={index} className="flex items-center space-x-2 text-white">
+                                        <FaCheck className="text-primary" />
+                                        <p>{feature}</p>
+                                    </li>
+                                ))}
+                            </ul>
                         </Fade>
                         <Fade top distance="20px" duration={1500} delay={600}>
                             <button className="bg-primary hover:bg-blue-600 hover:scale-110 transition-all shadow-lg duration-300 p-4 rounded-2xl text-l1 font-semibold lg:mx-0">View example website</button>

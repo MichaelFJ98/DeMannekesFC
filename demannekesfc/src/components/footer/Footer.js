e2c713667b476a6a63bd797f071d5d87e1617035
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2024-02-08 14:21:48 +0100

    Update Footer.js

--- demannekesfc/src/components/footer/Footer.js
+++ demannekesfc/src/components/footer/Footer.js
@@ -6,15 +6,14 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-800 text-white">
+    <footer className="bg-c2 text-white">
       <Fade duration={1500}>
         <div className="container mx-auto px-4 py-10">
-          {/* Ensures vertical stacking on mobile */}
-          <div className="flex flex-col items-center md:flex-row justify-between text-center md:text-left">
-            {/* Socials Section */}
-            <div className="mb-6 md:mb-0">
+          <div className="flex flex-col md:flex-row justify-between text-center md:text-left">
+            {/* Align items at the start for desktop */}
+            <div className="flex flex-col items-center md:items-start mb-6 md:mb-0 md:flex-1">
               <h2 className="text-lg font-semibold mb-4">Follow Us</h2>
-              <div className="flex gap-4 justify-center">
+              <div className="flex gap-4">
                 <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" className="text-white hover:text-blue-300">
                   <FaLinkedinIn size={24} />
                 </a>
@@ -26,14 +25,12 @@ export default function Footer() {
                 </a>
               </div>
             </div>
-            {/* Contact Us Section */}
-            <div className="text-center md:text-right">
+            <div className="flex flex-col items-center md:items-end md:flex-1">
               <h2 className="text-lg font-semibold mb-4">Contact Us</h2>
               <p className="text-sm">Vlaams-Brabant, 1930 Zaventem</p>
               <p className="text-sm mt-2">contact@insiter.be</p>
             </div>
           </div>
-          {/* Copyright Section */}
           <div className="border-t border-gray-700 mt-8 pt-6 text-center">
             <p className="text-xs">
               © {currentYear} Insiter. All rights reserved.
